23b3175aad6eadfa7170ddfeda2578adfd3ba8b4
Alex Langshur <adlangshur@gmail.com>
2019-12-25 14:26:09 -0500

    Setup Flask environment

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask.logging import create_logger
+import os
 
+# configure app
 app = Flask(__name__)
+log = create_logger(app)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+# run app
 if __name__ == '__main__':
     app.run()
